a1c0eee1359d5f3a172596fd9dd5646c4c720d64
Stephan Stoeber <stephan@cleverai.ch>
2025-04-08 16:54:19 +0200

    Fix: _document.tsx korrekt benannt für GTM

---
+++ pages/_document.tsx
@@ -0,0 +1,22 @@
+// pages/_document.tsx
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="de">
+      <Head />
+      <body>
+        <Main />
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5Q82VRX2"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
